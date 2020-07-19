ae97885eee41b141ada2f9232e5162db6de3b965
Keighley McFarland <keighley.mcfarland@kununu.com>
2020-07-19 16:04:59 +0200

    attempting to show sparkle but not working

--- src/Components/Main/MainComponent.jsx
+++ src/Components/Main/MainComponent.jsx
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Typist from 'react-typist';
+import spark from '../../assets/sparkle_gif.gif';
+
+import './styles.scss';
 
 const Main = () => {
+  const [displaySpark, setDisplaySpark] = useState(null);
+
+  const displaySparkTimer = () => {
+    setTimeout(7400, setDisplaySpark('displaySpark'));
+  };
+  useEffect(() => {
+    displaySparkTimer();
+  }, []);
+
   return (
     <div className='Main'>
-      <p>main main main</p>
+      <div className='headline'>
+        <h2>Full-stack web development with</h2>
+        <h2 className='typist-text'>
+          <Typist
+            cursor={{ hideWhenDone: true }}
+            avgTypingDelay={100}
+            startDelay={2000}
+          >
+            spark
+            <Typist.Backspace count={5} delay={1000} />
+            vision
+            <Typist.Backspace count={6} delay={1000} />
+            attention to detail
+            <Typist.Backspace count={4} delay={1000} />
+            <span></span>
+            <Typist.Backspace count={0} delay={1200} />
+            <span> ... hey look ma I'm typing!</span>
+            <Typist.Backspace count={43} delay={1200} />
+            spark
+          </Typist>
+          <img
+            src={spark}
+            className={displaySpark}
+            alt='spark icon - Icon made by Freepik from www.flaticon.com'
+          />
+        </h2>
+      </div>
+      <p>
+        I am a full-stack web developer specialised in Javascript, React,
+        Node.js, and Express.js
+      </p>
+      <p>
+        In my previous life, I was a project manager and science communications
+        specialist
+      </p>
+      <p>
+        I love building high-quality digital products tailored to users' and
+        clients' needs
+      </p>
+      <button>Contact</button>
+      <button>Reference projects</button>
     </div>
   );
 };
