458a34e8166bdd2e451638cfc3a79918f97e4197
Keighley McFarland <keighley.mcfarland@kununu.com>
2020-07-20 13:54:10 +0200

    align sticky bottom footer

--- src/Components/Footer/FooterComponent.jsx
+++ src/Components/Footer/FooterComponent.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import "./styles.scss"
 
 const Footer = () => {
   return (
-    <footer className="Footer">
-        Footer
+    <footer className="footer">
+        <span>(c) Keighley McFarland</span>
+        <span className="social">Contact</span>
+        <div>
+        <span className="social">Github</span>
+        <span className="social">LinkedIn</span>
+        </div>
     </footer>
   );
 }
