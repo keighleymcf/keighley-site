6a706c79d9c7990aeaf15b7ca23d5f0d76b04dc5
Keighley McFarland <keighley.mcfarland@kununu.com>
2020-07-20 14:45:27 +0200

    base for references component

--- src/Components/References/ReferencesComponent.jsx
+++ src/Components/References/ReferencesComponent.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const References = () => {
   return (
     <div className="References">
-        References
+        <h3>References</h3>
+        <section className="references-projects">
+        <h4>Experimentation and Learning Projects</h4>
+        <p>AppointerMed</p>
+        <p>sh*t i wanna eat</p>
+        <p>Peace Pal</p>
+        </section>
+        <section className="references-socials">
+        <h4>Socials</h4>
+        <a href="https://github.com/keighleymcf">Github</a>
+        <a href="https://www.linkedin.com/in/keighleymcfarland/">LinkedIn</a>
+        </section>
     </div>
   );
 }
