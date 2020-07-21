7aa204af777b0157d9059989dedacaa9e445bbeb
Keighley McFarland <keighley.mcfarland@kununu.com>
2020-07-21 20:17:50 +0200

    references formatting wip

--- src/Components/References/ReferencesComponent.jsx
+++ src/Components/References/ReferencesComponent.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './styles.scss';
 
-const References = () => {
-  return (
-    <div className="References">
-        <h3>References</h3>
-        <section className="references-projects">
-        <h4>Experimentation and Learning Projects</h4>
-        <p>AppointerMed</p>
-        <p>sh*t i wanna eat</p>
-        <p>Peace Pal</p>
-        </section>
-        <section className="references-socials">
-        <h4>Socials</h4>
-        <a href="https://github.com/keighleymcf">Github</a>
-        <a href="https://www.linkedin.com/in/keighleymcfarland/">LinkedIn</a>
-        </section>
-    </div>
-  );
-}
+const References = () => (
+  <div className="references">
+    <h3>References</h3>
+    <section className="references-projects">
+      <h4>Experimentation and Learning Projects</h4>
+      <p>AppointerMed</p>
+      <p>sh*t i wanna eat</p>
+      <p>Peace Pal</p>
+    </section>
+    <section className="references-socials">
+      <h4>Socials</h4>
+      <a href="https://github.com/keighleymcf">Github</a>
+      <a href="https://www.linkedin.com/in/keighleymcfarland/">LinkedIn</a>
+    </section>
+  </div>
+);
 
 export default References;
