68a87434e03f901cf8fa9d648f0e197c8689e046
Keighley McFarland <mcfarland.ke@gmail.com>
2021-03-24 18:28:12 +0100

    debugging og meta tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { IntlProvider } from 'react-intl';
-import {Helmet} from "react-helmet"
-
 import './app.scss';
 
 import Header from './Components/Header';
@@ -13,24 +11,11 @@ import Contact from './Components/Contact';
 import References from './Components/References';
 import Impressum from './Components/Impressum';
 
-import preview from "./assets/social-preview-image.png"
-
 const App = () => {
   const locale = 'en';
 
   return (
     <IntlProvider locale={locale}>
-    <Helmet>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta
-      name="description"
-      content="Keighley McFarland | Full-stack web development with spark"
-    />
-    <meta property="og:image" content={preview}/>
-    <meta property="og:image:type" content="image/png"/>
-    <meta property="og:image:width" content="1024"/>
-    <title>Keighley McFarland | Full-Stack Web Development</title>        </Helmet>
       <Router>
         <div className="App">
           <div className="main-content">
