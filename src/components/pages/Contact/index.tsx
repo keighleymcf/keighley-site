0859a3fec55b2e9022d1a9f085de6d4d9d8d87a6
Keighley McFarland <keighley.mcfarland@hi.health>
2022-05-19 12:31:44 +0200

    update some packages and lottie library

--- src/components/pages/Contact/index.tsx
+++ src/components/pages/Contact/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 
 import RonSign from "../../../assets/ron-poop-sign.gif";
 import ContactTYP from "./ContactTYP";
--- src/components/ui/LoadingIndicator/index.tsx
+++ src/components/ui/LoadingIndicator/index.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import Lottie from "react-lottie";
+import Lottie from "lottie-react";
 import * as animationData from "../../../assets/loader.json";
 
 export const LoadingIndicator = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
+  // const defaultOptions = {
+  //   loop: true,
+  //   autoplay: true,
+  //   animationData: animationData,
+  //   rendererSettings: {
+  //     preserveAspectRatio: "xMidYMid slice",
+  //   },
+  // };
 
-  return <Lottie options={defaultOptions} height={52} width={200} />;
+  return (
+    <Lottie
+      animationData={animationData}
+      loop={true}
+      autoplay={true}
+      height={52}
+      width={200}
+    />
+  );
 };
